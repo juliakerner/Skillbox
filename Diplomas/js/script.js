a2b1bdc476328b1b2433035bf5bc9a74ce8f9172
Julia Kerner <juliakerner@gmail.com>
2017-01-17 20:19:01 -0500

    Jambotron out, some css updates

--- Diplomas/js/script.js
+++ Diplomas/js/script.js
@@ -1,4 +1,24 @@
+/* Home page slide show
+var slideIndex1 = 0;
+showSlides1();
 
+function showSlides1() {
+    var i;
+    var slides = document.getElementsByClassName("homeSlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex1 = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex1-1].style.display = "block";  
+    dots[slideIndex1-1].className += " active";
+    setTimeout(showSlides1, 5000); // Change image every 5 seconds
+}
+*/
 /* Spinner */
 
 $(document).on('click', '.number-spinner button', function () {    
@@ -33,6 +53,45 @@ $(function () {
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+/** MODAL/Lightbox **/
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
 
 
 
