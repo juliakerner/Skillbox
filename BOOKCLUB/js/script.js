2902849eeaddf5dd27921a0af027017af436e346
Julia Kerner <juliakerner@gmail.com>
2017-03-15 23:50:37 -0400

    Bazhov page words presentation

--- BOOKCLUB/js/script.js
+++ BOOKCLUB/js/script.js
@@ -9,3 +9,14 @@ $(function () {
 function myCard() {
     document.getElementByClass("card-img").style.display = "inline";
 }
+
+// $('#zag').hover(
+// function(){$(this).toggleClass(‘animated hinge’);}
+// );
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("p").toggleClass("main");
+//     });
+// });
